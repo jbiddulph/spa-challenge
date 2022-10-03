ab2702c7f2101ee5c1615a514e07bea0ee4ae0f6
jbiddulph <john.mbiddulph@gmail.com>
2022-10-03 15:28:05 +0100

    added companies component and prepare for consuming api

---
+++ src/components/ListCompanies.vue
@@ -0,0 +1,13 @@
+<template>
+  <li class="max-w-100" v-for="company in companies" :key="company.id">
+    {{ company.first_name }}
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    companies: [],
+  },
+};
+</script>
--- src/views/CompanyView.vue
+++ src/views/CompanyView.vue
@@ -1,15 +1,27 @@
 <template>
   <NavBar />
   <h2>Company Page</h2>
+  <ul>
+    <ListCompanies :companies="companies" />
+  </ul>
 </template>
 
 <script>
 import NavBar from "@/components/NavBar.vue";
-
+import ListCompanies from "@/components/ListContacts.vue";
 export default {
   name: "CompanyPage",
   components: {
     NavBar,
+    ListCompanies,
+  },
+  methods: {
+    getAllCompanies() {
+      console.log("getting all company data...");
+    },
+  },
+  mounted() {
+    this.getAllCompanies();
   },
 };
 </script>
