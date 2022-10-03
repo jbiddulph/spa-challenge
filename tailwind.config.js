3f73789bb3b17290639db2323dfd8b8146b77326
jbiddulph <john.mbiddulph@gmail.com>
2022-10-03 23:02:40 +0100

    navbar sorted finally - still need to show hide meny on mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-orange': '#f29d4b',
+        'dark-orange': '#f29d4b',
+      },
+    },
   },
   plugins: [],
 };
